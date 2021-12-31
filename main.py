20c2491d81384939e8ea3024eb137296fb1c1b60
Okan <okso2004@gmail.com>
2021-12-31 22:07:09 +0100

    Worked on registration function

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
+from random import randint
+
+def create_account(name, birthday, balance, address):
+
+    # Generating the IBAN
+    iban = ""
+    for i in range(22):
+        iban += str(randint(0, 9))
+    IBAN = f"DE {int(iban)}"
+
+    # Creating a birthday string
+    x = 0
+    new_birthday = ""
+    for i in range(len(birthday)):
+        if (x % 2) == 0:
+            new_birthday += birthday[i] + "."
+        x += 1
+        print(new_birthday)
+    
+
+    # Saving information on a textfile
+    with open(f"{name}.txt", "w") as f:
+        f.write(f"Name: {name}\n")
+        f.write(f"Date of birth: {birthday}\n")
+        f.write(f"Address: {address}\n")
+        f.write(f"Balance: {balance}\n")
+        f.write(f"IBAN: {IBAN}\n")
+
+
+def log_in():
+    pass
+
+
+def withdraw():
+    pass
+
+
+def deposit():
+    pass
+
+
 def main():
     pass
 
 
 if __name__ == "__main__":
-    main()
+    create_account("test", "150108", 5000, "Germany")
+    # main()
 
