1de4c7ce5212ed809f1b77ad8b51b65e3b297e2b
Okan <okso2004@gmail.com>
2022-01-01 23:49:54 +0100

    Added login option

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from random import randint
 import os
-from types import CodeType
 
 
 def clear(): return os.system("clear") if os.name == "posix" else os.system("cls")
@@ -43,8 +42,26 @@ def create_account(name, birthday, balance, address):
         f.write(f"IBAN: {IBAN}\n")
 
 
-def log_in():
-    pass
+def log_in(name, input_code):
+    print(name)
+    try:
+        with open(f"{name}.txt", "r") as f:
+            lines = f.readlines()
+
+            balance_line = lines[4]
+            split_balance_line = balance_line.split(" ")
+            balance = split_balance_line[1].replace("\n", "")
+
+            code_line = lines[3]
+            split_code_line = code_line.split(" ")
+            code = int(split_code_line[1])
+
+        if input_code == code:
+            print(f"Success. Welcome, {name} your balance is {balance}.")
+        else:
+            print("Failure. Wrong code.")
+    except:
+        print("Account does not exist.")
 
 
 def withdraw():
@@ -61,10 +78,16 @@ def main():
 
 if __name__ == "__main__":
     clear()
-    create_account(
-        str(input("Enter your full name: ")),
-        str(input("Enter your birthday (ddmmyy): ")),
-        int(input("Enter your balance: ")),
-        str(input("Enter the country you live in: "))
+    # create_account(
+    #     str(input("Enter your full name: ")),
+    #     str(input("Enter your birthday (ddmmyy): ")),
+    #     int(input("Enter your balance: ")),
+    #     str(input("Enter the country you live in: "))
+    # )
+
+    log_in(
+        str(input("Enter your name: ")),
+        int(input("Enter four digit code: "))
     )
+
     # main()
